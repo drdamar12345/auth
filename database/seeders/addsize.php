f66444cdddf063f6469425b346ff6835a97093b0
drdamar12345 <135187492+drdamar12345@users.noreply.github.com>
2023-10-25 08:32:38 +0700

    langkah_pertama

---
+++ database/seeders/addsize.php
@@ -0,0 +1,456 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class addsize extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \DB::table('tb_size')->insert([
+            // [
+            //     'id_product' => '1',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '1',
+            //     'size' => '38',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '1',
+            //     'size' => '39',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '1',
+            //     'size' => '40',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '1',
+            //     'size' => '41',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '1',
+            //     'size' => '42',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '1',
+            //     'size' => '43',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '1',
+            //     'size' => '44',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '2',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '2',
+            //     'size' => '38',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '2',
+            //     'size' => '39',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '2',
+            //     'size' => '40',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '2',
+            //     'size' => '41',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '2',
+            //     'size' => '42',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '2',
+            //     'size' => '43',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '2',
+            //     'size' => '44',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '3',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '3',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '3',
+            //     'size' => '38',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '3',
+            //     'size' => '39',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '3',
+            //     'size' => '40',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '3',
+            //     'size' => '41',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '3',
+            //     'size' => '42',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '3',
+            //     'size' => '43',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '4',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '4',
+            //     'size' => '38',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '4',
+            //     'size' => '39',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '4',
+            //     'size' => '40',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '4',
+            //     'size' => '41',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '4',
+            //     'size' => '42',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '4',
+            //     'size' => '43',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '5',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '5',
+            //     'size' => '38',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '5',
+            //     'size' => '39',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '5',
+            //     'size' => '40',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '5',
+            //     'size' => '41',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '5',
+            //     'size' => '42',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '5',
+            //     'size' => '43',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '6',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '6',
+            //     'size' => '38',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '6',
+            //     'size' => '39',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '6',
+            //     'size' => '40',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '6',
+            //     'size' => '41',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '6',
+            //     'size' => '42',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '6',
+            //     'size' => '43',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '7',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '7',
+            //     'size' => '38',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '7',
+            //     'size' => '39',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '7',
+            //     'size' => '40',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '7',
+            //     'size' => '41',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '7',
+            //     'size' => '42',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '7',
+            //     'size' => '43',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '8',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '8',
+            //     'size' => '38',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '8',
+            //     'size' => '39',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '8',
+            //     'size' => '40',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '8',
+            //     'size' => '41',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '8',
+            //     'size' => '42',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '8',
+            //     'size' => '43',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '9',
+            //     'size' => '37',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '9',
+            //     'size' => '38',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '9',
+            //     'size' => '39',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '9',
+            //     'size' => '40',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '9',
+            //     'size' => '41',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '9',
+            //     'size' => '42',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            // [
+            //     'id_product' => '9',
+            //     'size' => '43',
+            //     'stok' => '15',
+            //     'status' => 'tersedia',
+            // ],
+            [
+                'id_product' => '3',
+                'size' => '44',
+                'stok' => '15',
+                'status' => 'tersedia',
+            ],
+            [
+                'id_product' => '4',
+                'size' => '44',
+                'stok' => '15',
+                'status' => 'tersedia',
+            ],
+            [
+                'id_product' => '5',
+                'size' => '44',
+                'stok' => '15',
+                'status' => 'tersedia',
+            ],
+            [
+                'id_product' => '6',
+                'size' => '44',
+                'stok' => '15',
+                'status' => 'tersedia',
+            ],
+            [
+                'id_product' => '7',
+                'size' => '44',
+                'stok' => '15',
+                'status' => 'tersedia',
+            ],
+            [
+                'id_product' => '8',
+                'size' => '44',
+                'stok' => '15',
+                'status' => 'tersedia',
+            ],
+            [
+                'id_product' => '9',
+                'size' => '44',
+                'stok' => '15',
+                'status' => 'tersedia',
+            ],
+        ]);
+    }
+}
